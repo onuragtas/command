2b8ded93964c374ec9b092007d0610d7d9b867ab
onuragtas <agtasonur@gmail.com>
2023-12-17 13:43:00 +0300

    command changes

--- command.go
+++ command.go
@@ -3,17 +3,16 @@ package command
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
+	"os"
 	"os/exec"
 	"time"
 )
 
 type Command struct {
-	StdOutWriter  func([]byte)
-	StdErrWriter  func([]byte)
-	StdInData     string
-	Sleep         time.Duration
-	OutputAndQuit bool
+	stdInFunction func()
+	stdInDuration int
 }
 
 func (c *Command) Run(cmd string) ([]byte, error) {
@@ -21,89 +20,62 @@ func (c *Command) Run(cmd string) ([]byte, error) {
 }
 
 func (t *Command) RunCommand(path string, name string, arg ...string) {
+	prout, pwout := io.Pipe()
+	prerr, pwerr := io.Pipe()
+
 	cmd := exec.Command(name, arg...)
 	fmt.Println("command:", name, arg)
 	if path != "" {
 		cmd.Dir = path
 	}
+	cmd.Stdout = pwout
+	cmd.Stderr = pwerr
 
-	var bout, berr bytes.Buffer
-	cmd.Stdout = &bout
-	cmd.Stderr = &berr
-
-	// Komutun stdin'e yazılması
-	if t.StdInData != "" {
-		in, err := cmd.StdinPipe()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		go func() {
-			defer in.Close()
-			in.Write([]byte(t.StdInData))
-		}()
-	}
-
-	c := make(chan bool)
-	cError := make(chan bool)
+	tout := io.TeeReader(prout, os.Stdout)
+	terr := io.TeeReader(prerr, os.Stderr)
 
 	if err := cmd.Start(); err != nil {
-		c <- true
-		cError <- true
+		log.Fatal(err)
 	}
 
+	var bout, berr bytes.Buffer
+
 	go func() {
-		loop := true
-		for loop {
-			select {
-			case <-c:
-				loop = false
-				break
-			default:
-				// Çıktıları yakalayın ve bir işleve gönderin
-				loop = t.send(bout, t.StdOutWriter)
-			}
+		if _, err := io.Copy(&bout, tout); err != nil {
+			log.Fatal(err)
 		}
 	}()
 
 	go func() {
-		loop := true
-		for loop {
-			select {
-			case <-cError:
-				loop = false
-				break
-			default:
-				// Çıktıları yakalayın ve bir işleve gönderin
-				loop = t.send(berr, t.StdErrWriter)
-			}
+		if _, err := io.Copy(&berr, terr); err != nil {
+			log.Fatal(err)
 		}
 	}()
 
 	if err := cmd.Wait(); err != nil {
-		c <- true
-		cError <- true
+		log.Println(err)
 	}
 
-	c <- true
-	cError <- true
+	fmt.Printf("buffered out %s\n", bout.String())
+	fmt.Printf("buffered err %s\n", berr.String())
+}
+func (t *Command) RunWithPipe(name string, args ...string) {
+	cmd := exec.Command(name, args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
 
-	t.send(bout, t.StdOutWriter)
-	t.send(berr, t.StdErrWriter)
+	go func() {
+		if t.stdInFunction != nil {
+			time.Sleep(time.Duration(t.stdInDuration) * time.Second)
+			t.stdInFunction()
+		}
+	}()
+
+	err := cmd.Run()
+	fmt.Println(err)
 }
 
-func (t *Command) send(buf bytes.Buffer, writer func([]byte)) bool {
-	loop := true
-	output := buf.Bytes()
-	if writer != nil && len(output) > 0 {
-		writer(output)
-		if t.OutputAndQuit {
-			loop = false
-		}
-	}
-	if t.Sleep > 0 {
-		time.Sleep(t.Sleep * time.Millisecond)
-	}
-
-	return loop
+func (t *Command) AddStdIn(duration int, f func()) {
+	t.stdInFunction = f
+	t.stdInDuration = duration
 }
